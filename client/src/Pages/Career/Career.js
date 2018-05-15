756c424aa37a554f503a7a6fcafecee019715551
rupali.meshram <meshram.rupali86@gmail.com>
2018-05-15 16:52:30 -0400

    code clean up , setting of scroll , changes in footer ,comment code lines

--- client/src/Pages/Career/Career.js
+++ client/src/Pages/Career/Career.js
@@ -15,72 +15,66 @@ class Career extends Component {
         this.state = {
             isActive:false
           }
-  }     
+    }     
      
-   componentWillMount(){
-        // Modal.setAppElement('body');
-    }
-
-    toggleModal = () => {
+   toggleModal = () => {
       this.setState({
       isActive:!this.state.isActive
      })
     }
 
-
-  render() {
-    // inline sty
-     const imagesetting = {
-      "width" : "100%",
-       "marginTop": "-150px",
-      "height" : "auto"
-    }
-    
-    const jumbotronsetting = {
-      "background-color" : "white"
-    }
+render() {
+    // inline style
+       const imagesetting = {
+        "width" : "100%",
+         "marginTop": "-150px",
+        "height" : "auto"
+      }
+      
+      const jumbotronsetting = {
+        "background-color" : "white"
+      }
     
 
     return (
       <div>
+        
         <Navbar/>
         <div className="row">
-
+            {/*image*/}
             <div className="col-4 imageside">
               <img  alt="image2" src={require("../../images/career.jpg")} style={imagesetting}/>
             </div>
-
+            {/*about career*/}
             <div className="col-8 jobDetailSide">
               <div className="jumbotron jumbotron-fluid" style={jumbotronsetting}>
-            <div className="container">
-              <h2 className="display-4">Connecting People.Improving Skills.</h2><hr/>
-              <p className="lead">Youtism is a non-profit organization that exists to educate and connect
-               individuals and families to answers that matter: information and resources that help them make more informed decisions, 
-               build and strengthen relationships, and more importantly, thrive. Together, we’ll help you connect the pieces.</p>
+                <div className="container">
 
-              <Imagegrid/>
-            
-                <button type="button" className="btn btn-dark btn-lg btn-block" onClick={this.toggleModal}><strong>POST JOBS HERE</strong></button>
-                
-                <Modal show={this.state.isActive} onHide={this.toggleModal}>
-
-                  <Postjob />
-                  
-                </Modal>
-
-
-              <button type="button" className="btn btn-primary btn-lg btn-block"><strong>FIND JOBS HERE</strong></button>
-             </div>
-           </div>
-         </div>
-        </div>
-            
-
-       
-        <Footer/>
-       </div>
-
-        
+                    <h2 className="display-4">Connecting People.Improving Skills.</h2><hr/>
+                    <p className="lead">Youtism is a non-profit organization that exists to educate and connect
+                     individuals and families to answers that matter: information and resources that help them make more informed decisions, 
+                     build and strengthen relationships, and more importantly, thrive. Together, we’ll help you connect the pieces.</p>
+                    
+                    {/*3 images*/}
+                    <Imagegrid/>
+                    
+                    {/*toggle button for post jobs*/}
+                    <button type="button" className="btn btn-dark btn-lg btn-block" onClick={this.toggleModal}><strong>POST JOBS HERE</strong></button>
+                      
+                      {/*post job form will open in Modal*/}
+                      <Modal show={this.state.isActive} onHide={this.toggleModal}>
+                          <Postjob />
+                      </Modal>
+                      {/*Job list will show up on click */}
+                    <button type="button" className="btn btn-primary btn-lg btn-block"><strong>FIND JOBS HERE</strong></button>
+                </div>
+              </div>
+            </div>
+          </div> {/*end about career*/}
+           
+           {/*footer*/} 
+          <Footer/>
+       </div>        
     );
   }
 }
---
+++ client/src/components/Guestlogin/Guestlogin.js
@@ -0,0 +1,75 @@
+import React, { Component } from "react";
+
+import { Input, FormBtn } from "../../components/Form";
+
+import "../../components/Form/Modalsetting.css";
+
+class Guestlogin extends Component {
+
+  constructor(props) {
+      super(props); 
+
+        this.state = {
+
+          loginEmail: "",
+          loginPassword: ""
+
+        }
+    
+      this.loginSubmit = this.loginSubmit.bind(this);
+      this.loginChange = this.loginChange.bind(this);
+    };
+
+    loginSubmit = (e) => { 
+      e.preventDefault();
+
+      alert("Your login credentials have been submitted! Details: " + JSON.stringify(this.state));
+      this.setState({
+        
+          loginEmail: "",
+          loginPassword: ""
+       })
+
+    } 
+
+
+    loginChange = (e) => {
+      this.setState({[e.target.name]: e.target.value});
+    }
+
+    render () {
+
+      const {loginEmail, loginPassword} = this.state;
+
+      return (
+
+          <form onSubmit={this.loginSubmit}>
+                          <label for="loginEmail"> Email address</label>
+                            <Input 
+                            name="loginEmail"
+                            id="loginEmail" 
+                            placeholder="johnsmith@email.com"
+                            value={loginEmail}
+                            onChange={this.loginChange} />
+
+                          <label for="loginPassword"> Password </label>
+                            <Input 
+                            name="loginPassword"
+                            id="loginPassword" 
+                            placeholder="Password"
+                            value={loginPassword}
+                            onChange={this.loginChange} />
+
+ 
+                         <FormBtn type="submit"> Log In </FormBtn>
+                         
+          </form>               
+
+      );
+    }
+}
+
+export default Guestlogin;
+
+
+  
